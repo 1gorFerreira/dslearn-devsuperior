1f717d8e372da0f14ba43a55fa407bf1d76d470f
Igor <igorferreiradaniel99@hotmail.com>
2022-06-13 21:18:16 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2021/03/18/17/09/teacher-6105336_960_720.png', 'https://images.squarespace-cdn.com/content/v1/5817787a59cc689660baf24b/1590422055281-GCLKECLNM5MPZFOSMNRK/GP-icon-6.png');
